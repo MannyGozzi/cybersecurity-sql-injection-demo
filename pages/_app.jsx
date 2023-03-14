99d9f6f1be6b1c65af385f5cca68780b9d160177
Manuel Gozzi <manuelgozzi@Gozzi-M1.local>
2023-03-14 09:24:24 -0700

    Changed website title

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,12 +1,16 @@
 import { SessionProvider } from "next-auth/react"
 import 'styles/globals.css'
 import Navbar from 'components/Navbar'
+import Head from 'next/head'
 
 export default function App({
   Component, pageProps: { session, ...pageProps }
 }) {
   return (
     <SessionProvider session={session}>
+      <Head>
+        <title>Notably Bad</title>
+      </Head>
       <Navbar/>
       <Component {...pageProps}/>
     </SessionProvider>
